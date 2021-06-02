fe678d890a703e58d5896d2193fe00897e580be6
BorgePambo <borginhopambo01@gmail.com>
2021-06-02 11:28:31 -0300

    Meu Projeto

---
+++ src/javaapp03/JavaApp03.java
@@ -0,0 +1,16 @@
+package javaapp03;
+
+public class JavaApp03 {
+
+    
+    public static void main(String[] args) {
+      
+        String nomes[] = {"Alex", "Adriano", "Borge", "Carlos", "Dadinho"};
+        
+        for(String x: nomes){
+            System.out.println(x);
+        }
+        
+    }
+    
+}
